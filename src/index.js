b89624914c08753c2591ceaad92e8ec6dfefb3c7
Nghia <hello.nghia22@gmail.com>
2021-04-05 15:18:00 -0400

    style: Add base styles

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom";
-import App from 'components/App'
+import App from 'components/App';
+import "css/index.css"
 
 ReactDOM.render(<App />, document.querySelector("#root"));
