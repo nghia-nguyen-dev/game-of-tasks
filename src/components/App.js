97622c1cba3f85ce4af64e48368280c909b1ba78
Nghia <hello.nghia22@gmail.com>
2021-04-05 19:45:36 -0400

    style: Filter

--- src/components/App.js
+++ src/components/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-import TaskBar from 'components/TaskBar'
-import Filter from 'components/Filter'
+import TaskBar from "components/TaskBar";
+import Filter from "components/Filter";
 
 const App = () => {
-	return <div className="App"><TaskBar/><Filter/></div>;
+	return (
+		<div className="App">
+			<TaskBar />
+			<Filter />
+		</div>
+	);
 };
 
 export default App;
--- src/components/Filter.js
+++ src/components/Filter.js
@@ -4,7 +4,19 @@ const Filter = () => {
     return (
         <div className="Filter">
             <h2 className="Filter--label">Filter</h2>
-            {/* <div className="Filter--divider"></div> */}
+            <div className="Status">
+                <p>all</p>
+                <p>complete</p>
+                <p>todo</p>
+            </div>
+            <div className="Actions">
+                <p>Check all</p>
+                <p>Clear</p>
+            </div>
+            <div className="Tracker">
+                <p>Remaining</p>
+                <p className="Tracker--number">4</p>
+            </div>
         </div>
     )
 }
