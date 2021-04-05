0928e89ed8011d8555ee945829294bbdd73bfa41
Nghia <hello.nghia22@gmail.com>
2021-04-05 17:38:37 -0400

    feat: Add Filter component

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from "react";
 import TaskBar from 'components/TaskBar'
+import Filter from 'components/Filter'
 
 const App = () => {
-	return <div className="App"><TaskBar/></div>;
+	return <div className="App"><TaskBar/><Filter/></div>;
 };
 
 export default App;
---
+++ src/components/Filter.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Filter = () => {
+    return (
+        <div className="Filter">
+            <div className="Filter--divider"></div>
+        </div>
+    )
+}
+
+export default Filter
