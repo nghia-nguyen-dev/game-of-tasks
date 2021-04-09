d31cc969e994b8220b0ceafa1e6461f4df4fba08
Nghia <hello.nghia22@gmail.com>
2021-04-09 00:39:09 -0400

    refactor: Abstract out different task views

--- src/components/Item.js
+++ src/components/Item.js
@@ -2,55 +2,22 @@ import React, { useContext, useState, useEffect } from "react";
 import DeleteIcon from "components/DeleteIcon";
 import EditIcon from "components/EditIcon";
 import TaskContext from "utils/TaskContext";
-import { toggleTask, updateTask } from "utils/actions";
+import TaskDefault from "components/TaskDefault";
+import TaskInput from "components/TaskInput";
 
 const Item = ({ id, title, isComplete }) => {
-	const { state, dispatch } = useContext(TaskContext);
-	const [input, setInput] = useState("");
+	const { state } = useContext(TaskContext);
 
-	useEffect(() => {
-		if (state.edit.isOn) {
-			document.addEventListener("click", handleSubmit);
-			return () => document.removeEventListener("click", handleSubmit);
-		}
-	}, [input]);
-
-	const handleSubmit = e => {
-		e.preventDefault();
-		dispatch(updateTask(input, id));
-		setInput("");
-	};
-
-	const handleChange = e => setInput(e.target.value);
-	const handleClick = () => dispatch(toggleTask(id));
-
-	const defaultView = (
-		<p
-			className={`Item__text ${isComplete && "Item__complete"}`}
-			onClick={handleClick}
-		>
-			{title}
-		</p>
-	);
-
-	const inputView = (
-		<form onSubmit={handleSubmit}>
-			<input
-				autoFocus
-				type="text"
-				onChange={handleChange}
-				value={input}
-				className="Item__input"
-				placeholder={title}
-			></input>
-		</form>
-	);
+	const renderedTask =
+		state.edit.id === id && state.edit.isOn ? (
+			<TaskInput title={title} id={id} />
+		) : (
+			<TaskDefault id={id} isComplete={isComplete} title={title} />
+		);
 
 	return (
 		<li className="Item">
-			{state.edit.id === id && state.edit.isOn
-				? inputView
-				: defaultView}
+			{renderedTask}
 			<div className="Todos-icons">
 				<EditIcon id={id} isComplete={isComplete} />
 				<DeleteIcon id={id} />
---
+++ src/components/TaskInput.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect, useContext } from "react";
+import TaskContext from "utils/TaskContext";
+import { updateTask } from "utils/actions";
+
+const TaskInput = ({ title, id }) => {
+	const { state, dispatch } = useContext(TaskContext);
+	const [input, setInput] = useState("");
+
+	useEffect(() => {
+		if (state.edit.isOn) {
+			document.addEventListener("click", handleSubmit);
+			return () => document.removeEventListener("click", handleSubmit);
+		}
+	}, [input]);
+
+	const handleSubmit = e => {
+		e.preventDefault();
+		dispatch(updateTask(input, id));
+		setInput("");
+	};
+
+	const handleChange = e => setInput(e.target.value);
+
+	return (
+		<form onSubmit={handleSubmit}>
+			<input
+				autoFocus
+				type="text"
+				onChange={handleChange}
+				value={input}
+				className="Item__input"
+				placeholder={title}
+			></input>
+		</form>
+	);
+};
+
+export default TaskInput;
