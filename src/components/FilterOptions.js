8279aedb2ea7121889047381e67febaf4c6db650
Nghia <hello.nghia22@gmail.com>
2021-04-09 10:25:10 -0400

    feat: Implement logic to get count for each filter

--- src/components/FilterOptions.js
+++ src/components/FilterOptions.js
@@ -8,15 +8,35 @@ const FilterOptions = () => {
 	const { state } = useContext(TaskContext);
 
 	const getCount = option => {
-		return 1;
+		switch (option) {
+			case ALL:
+				return state.tasks.length;
+
+			case COMPLETE:
+				return state.tasks.filter(task => task.isComplete).length;
+
+			case TODO:
+				return state.tasks.filter(task => !task.isComplete).length;
+
+			default:
+				return 0;
+		}
 	};
 
-	const isActive = option => state.show === option ? "active" : null;
+	const isActive = option => (state.show === option ? "active" : null);
 
 	const renderedOptions = options.map(option => (
 		<li className={`Option`} key={option}>
-			<p className={`Option__label Option__label--${isActive(option)}`}>{option}</p>
-			<p className={`Option__counter Option__counter--${isActive(option)}`}>{getCount()}</p>
+			<p className={`Option__label Option__label--${isActive(option)}`}>
+				{option}
+			</p>
+			<p
+				className={`Option__counter Option__counter--${isActive(
+					option
+				)}`}
+			>
+				{getCount(option)}
+			</p>
 		</li>
 	));
 
